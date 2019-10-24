05b3c19304f3498de8e4333c029b91b5ff0fb6b0
Sofie Lund Mogensen <sofie_6940@hotmail.com>
2019-10-24 19:23:02 +0200

    Add project files.

---
+++ Contronic/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+
+namespace Contronic.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+
+        public ActionResult MailSender(string userEmail, string subject, string name, string message)
+        {
+            MailMessage mail = new MailMessage();
+
+            SmtpClient Smtp = new SmtpClient()
+            {
+                Host = "smtp.unoeuro.com",
+                EnableSsl = true,
+                Port = 587,
+                Credentials = new NetworkCredential("info@contronic.dk", "6940lemst")
+            };
+
+            mail.From = new MailAddress("info@contronic.dk");
+            mail.To.Add("info@contronic.dk");
+            mail.Subject = "Mail fra Contronic - Kontakt";
+            mail.IsBodyHtml = true;
+
+            mail.Body = "Name:" + name +
+                "<br /> Mail: " + userEmail +
+                "<br /><br />Besked:" + message;
+
+            Smtp.Send(mail);
+
+            return RedirectToUmbracoPage(1052, "status=sent");
+        }
+
+        public ActionResult InstalSender(string firm, string phone, string name, string userEmail, string City, string message)
+        {
+            MailMessage mail = new MailMessage();
+
+            SmtpClient Smtp = new SmtpClient()
+            {
+                Host = "smtp.unoeuro.com",
+                EnableSsl = true,
+                Port = 587,
+                Credentials = new NetworkCredential("info@contronic.dk", "6940lemst")
+            };
+
+            mail.From = new MailAddress(userEmail);
+            mail.To.Add("info@contronic.dk");
+            mail.Subject = "Mail fra Contronic - Installatør";
+            mail.IsBodyHtml = true;
+             
+            
+            mail.Body = "Firma:" + firm +
+                "<br /> Navn: " + name +
+                "<br /> Telefon: " + phone +
+                "<br /> Email: " + userEmail +
+                "<br /> Område: " + City +
+                "<br /> Mail: " + message;
+
+            Smtp.Send(mail);
+
+            return RedirectToUmbracoPage(1052, "status=sent");
+        }
+    }
+}
---
+++ Contronic/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Contronic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Contronic")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e3f2c4d3-2b7c-4545-bc80-761740a1dc31")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
